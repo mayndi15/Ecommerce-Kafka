9e77343b7ebfe2a29658cf7747a5c53dceeb990a
mayndi15 <mayndi15@gmail.com>
2023-10-06 18:14:18 -0300

    service-email

---
+++ service-email/src/main/java/br/com/ecommerce/EmailService.java
@@ -0,0 +1,36 @@
+package br.com.ecommerce;
+
+import br.com.ecommerce.kafka.KafkaConsumers;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+
+public class EmailService {
+
+    public static void main(String[] args) {
+        var emailService = new EmailService();
+        try (var service = new KafkaConsumers<>(EmailService.class.getSimpleName(),
+                "ecommerce.send.email",
+                emailService::parse,
+                String.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("-------------------------------------------");
+        System.out.println("Send email...");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Email sent!");
+    }
+}
